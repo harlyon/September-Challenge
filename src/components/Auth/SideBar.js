125d1eb714caec94592adc42266b73f7ed68739b
Harrison <harrison.ekpobimi@sov.tech>
2019-12-29 21:21:28 +0200

    fix: increased height of sidebar

--- src/components/Auth/SideBar.js
+++ src/components/Auth/SideBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SideBar = () => {
   return (
       <div className="col-md-2">
-        <nav className="sidenav d-flex flex-column mb-5 mb-md-0">
+        <nav className="sidenav d-flex flex-column mt-4 mb-5 mb-md-0">
           <a className="text-uppercase text-xs mb-2" href="/dashboard">
             Dashboard
           </a>
